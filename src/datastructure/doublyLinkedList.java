c25f6341c7838679c83f30e29d8b7e633dc9061b
aarsh64 <48687891+aarsh64@users.noreply.github.com>
2020-05-09 12:54:27 -0700

    doubleyLinkedList Update

---
+++ src/datastructure/doublyLinkedList.java
@@ -0,0 +1,57 @@
+package datastructure;
+
+public class doublyLinkedList {
+	public linkedList head;
+	public linkedList tail;
+	public int length;
+	
+	public doublyLinkedList() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;	
+	}
+	
+	public boolean isEmpty() {
+		return length == 0;
+	}
+public void addNode(int d) {
+	linkedList n = new linkedList(d);
+	if(isEmpty()) {
+		tail = n;
+	}
+	else {
+		head.previous =  n;
+	}
+	n.next = head;
+	head = n;
+	length++;
+}
+public void printData() {
+	if (head == null) {
+		return;
+	}
+	else
+	{
+		linkedList temp = head;
+		while(temp != null) {
+			System.out.println("data is :"+ temp.data);
+			temp= temp.next;
+		}
+		
+		System.out.println("-----------------");
+	}
+}
+	
+public static void main(String[] args) {
+	doublyLinkedList L = new doublyLinkedList();
+	System.out.println(L.isEmpty());
+	
+	L.addNode(5);
+	L.addNode(10);
+	L.addNode(15);
+	L.printData();
+	System.out.println(L.isEmpty());
+	
+
+}
+}
---
+++ src/datastructure/linkedList.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+public class linkedList {
+	linkedList previous = null;
+	linkedList next = null;
+	int data;
+	
+	public linkedList(int d){
+		this.data = d;
+	}
+	
+
+}
