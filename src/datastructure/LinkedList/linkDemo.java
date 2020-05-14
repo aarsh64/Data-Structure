22683aedc91de396e23bb93e2e0241bb39c59f65
aarsh64 <48687891+aarsh64@users.noreply.github.com>
2020-05-14 14:56:25 -0700

    linkedList 1.14 //optimsed interSectionNode

--- src/datastructure/LinkedList/linkDemo.java
+++ src/datastructure/LinkedList/linkDemo.java
@@ -37,6 +37,9 @@ public class linkDemo{
 		}
 		return l;
 	}
+	void getNoder() {
+			System.out.println("Optimsed way: "+interSectionNode(head1,head2));
+	}
 	int interSectionNode(int x,Node n1,Node n2) {
 		
 		Node node1=n1;
@@ -57,8 +60,43 @@ public class linkDemo{
 		}
 		return -1;
 	}
-
+//Optimised compare to above one...
 	
+	 int interSectionNode(Node n1,Node n2) {
+		int l1=0,l2=0,d=0;
+		Node node1=n1,node2=n2;
+		while(node1!=null) {
+			l1++;
+			node1=node1.next;
+		}
+		while(node2!=null) {
+			l2++;
+			node2=node2.next;
+		}
+		if(l1<l2) {
+			node1=n2;
+			node2=n1;
+			d=l2-l1;
+		}
+		else {
+			node1=n1;
+			node2=n2;
+			d=l1-l2;
+		}
+		for(int i=0;i<d;i++) 
+			node1=node1.next;
+		
+		while(node1!=null && node2!=null) {
+			if(node1.data==node2.data) {
+					return node1.data;
+				}
+				node1=node1.next;
+				node2=node2.next;
+			}
+		
+		return -1;
+	}
+	 
 	public static void main(String[] args) {
 		
 		linkDemo l1= new linkDemo();
@@ -73,5 +111,6 @@ public class linkDemo{
         l1.head2.next = new Node(15); 
         l1.head2.next.next = new Node(30);
 		System.out.println("Intersction Node is :"+l1.getNode());
+		l1.getNoder();
 	}
 }
